766a8a9b445285af05acf71ee4307317522636d6
RubenVO <rubenvanostaeyen@hotmail.com>
2015-10-27 17:58:45 +0100

    oplossing include/require

---
+++ Oplossing-include-require/index.php
@@ -0,0 +1,23 @@
+<?php
+
+	var_dump( $_GET );
+
+		$artikels =	[	array(	"Titel" => "lenny", 
+								"Text" => "( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)",  
+								"Tag" => "le lenny face"), 
+
+					array(	"Titel" => "Brotherman Bill", 
+							"Text" => "KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona KKona ",
+							"Beschrijving" => "The brother living at the top of the hill"), 
+
+					array(	"Titel" => "Kappa", 
+							"Text" => "Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo Kappa KappaPride Keepo ",  
+							"Beschrijving" => "Grey Face no spacerino Kappa")
+				];
+
+
+	include 'view/header-partial.html';
+	include 'view/body-partial.html';
+	include 'view/footer-partial.html';
+	
+?>
